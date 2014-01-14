274cfbd5a8207b8cf693f9fc118940adc1662ab1
Alek Grinberg <agrinb@gmail.com>
2014-01-14 14:05:07 -0500

    initial poweredbyinspiration commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poweredbyinspiration::Application.config.secret_key_base = '8941b852dff0236bec42c31e0f82b37e5e4b95c0823da29c7370871bc29158c933c45d0d11e550d6274010c29818e6a9669bcaf05d10e0d4912d404e783cf24a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Poweredbyinspiration::Application.config.session_store :cookie_store, key: '_poweredbyinspiration_session'
